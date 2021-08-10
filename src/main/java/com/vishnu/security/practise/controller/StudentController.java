5b7fdc231c2bcdb8505b152188dfe6d982f40079
vishnuram <vmuralidharan@nextsphere.com>
2021-08-10 19:39:59 +0530

    Added Sample endpoints

--- src/main/java/com/vishnu/security/practise/controller/StudentController.java
+++ src/main/java/com/vishnu/security/practise/controller/StudentController.java
@@ -1,7 +1,15 @@
 package com.vishnu.security.practise.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/admin")
 public class StudentController {
+
+    @GetMapping("test")
+    String testMethof(){
+        return "asdfdv";
+    }
 }
