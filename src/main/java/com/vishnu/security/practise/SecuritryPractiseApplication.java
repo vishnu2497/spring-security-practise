9a79e476e350d2e5b80e3fa92db2ab1e46bc6bfa
vishnuram <vmuralidharan@nextsphere.com>
2021-08-10 08:40:54 +0530

    initial project setup

---
+++ src/main/java/com/vishnu/security/practise/SecuritryPractiseApplication.java
@@ -0,0 +1,13 @@
+package com.vishnu.security.practise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritryPractiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuritryPractiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vishnu/security/practise/SecuritryPractiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vishnu.security.practise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritryPractiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
