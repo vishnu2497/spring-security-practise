153595faa631cfaca1138b61b539d23177dfd084
vishnuram <vmuralidharan@nextsphere.com>
2021-08-10 19:36:18 +0530

    Various ways to create user data

--- src/main/java/com/vishnu/security/practise/config/SecurityConfig.java
+++ src/main/java/com/vishnu/security/practise/config/SecurityConfig.java
@@ -4,19 +4,35 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @EnableWebSecurity(debug = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("vishnuram").password("{bcrypt}$2a$12$DmncO3R9anLwApc5/CZPOOlYJlxbGNJnxTS3LhIgKUPfXCgsnBG9W").roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("vishnuram").password("{noop}vvv").roles("ADMIN");
     }
 
     @Bean
     public PasswordEncoder encoderP(){
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+
+    @Bean
+    public UserDetailsService abcs(){
+        UserDetails vvU = User.builder()
+                .username("vv")
+                .password("{noop}asas")
+                .roles("QWER")
+                .build();
+
+        return new InMemoryUserDetailsManager(vvU);
     }
 }
 
