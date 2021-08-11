34c3aee6a031605c54428525a3773e2ae82c863b
vishnuram <vmuralidharan@nextsphere.com>
2021-08-11 20:53:31 +0530

    Need to check once issue while using @EnableGlobalWebSecurity generated passwrod is creating

--- src/main/java/com/vishnu/security/practise/SecuritryPractiseApplication.java
+++ src/main/java/com/vishnu/security/practise/SecuritryPractiseApplication.java
@@ -2,8 +2,10 @@ package com.vishnu.security.practise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecuritryPractiseApplication {
 
 	public static void main(String[] args) {
