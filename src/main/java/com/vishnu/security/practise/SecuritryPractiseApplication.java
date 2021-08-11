55168fb7d2cc4ce738b1014c7ae6dab1700485ce
vishnuram <vmuralidharan@nextsphere.com>
2021-08-11 23:38:34 +0530

    Excluded the UserDetailsServiceAutoConfiguration class form the initial loading

--- src/main/java/com/vishnu/security/practise/SecuritryPractiseApplication.java
+++ src/main/java/com/vishnu/security/practise/SecuritryPractiseApplication.java
@@ -1,11 +1,14 @@
 package com.vishnu.security.practise;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
-//@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableAutoConfiguration(exclude = UserDetailsServiceAutoConfiguration.class)
 public class SecuritryPractiseApplication {
 
 	public static void main(String[] args) {
